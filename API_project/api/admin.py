556365a08f250921f1be0d76b2da24cd63f18bb9
IliasBarmpar <ilias.barbar.94@gmail.com>
2021-04-04 12:23:46 +0300

    -

--- API_project/api/admin.py
+++ API_project/api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Article
 
-#admin.site.register(Article)
+# Alternative 1
+# admin.site.register(Article)
 
+
+# Alternative 2
 @admin.register(Article)
 class ArticleModel(admin.ModelAdmin):
     list_filter = ('title', 'description')
